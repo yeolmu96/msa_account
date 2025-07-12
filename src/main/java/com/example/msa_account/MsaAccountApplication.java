6fbdd34ccb3fb553893a2edb3bc3700c275543c1
yeolmu96 <jeong7653@naver.com>
2025-07-12 17:31:31 +0900

    first commit message

---
+++ src/main/java/com/example/msa_account/MsaAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msa_account/MsaAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
